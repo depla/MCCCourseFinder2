5dea2464fc81446476d5c364ceb135a936eac384
Dennis La <dennisla95@yahoo.com>
2019-04-29 10:57:14 -0700

    update stuff

--- app/src/main/java/edu/miracostacollege/cs134/mcccoursefinder/MainActivity.java
+++ app/src/main/java/edu/miracostacollege/cs134/mcccoursefinder/MainActivity.java
@@ -21,6 +21,14 @@ import edu.miracostacollege.cs134.mcccoursefinder.model.DBHelper;
 import edu.miracostacollege.cs134.mcccoursefinder.model.Instructor;
 import edu.miracostacollege.cs134.mcccoursefinder.model.Offering;
 
+/**
+ * MCC Course Finder 2 - Lets user refine and search cs courses at MCC
+ *
+ * Dennis La
+ * 
+ * CS134
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     private DBHelper db;
@@ -56,8 +64,8 @@ public class MainActivity extends AppCompatActivity {
         filteredOfferingsList = new ArrayList<>(allOfferingsList);
 
 
-        //TODO (1): Construct instructorSpinnerAdapter using the method getInstructorNames()
-        //TODO: to populate the spinner.
+        //DONE (1): Construct instructorSpinnerAdapter using the method getInstructorNames()
+        //DONE: to populate the spinner.
         instructorSpinner = findViewById(R.id.instructorSpinner);
         final ArrayAdapter<String> instructorSpinnerAdapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item, getInstructorNames());
@@ -148,9 +156,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //TODO (2): Create a method getInstructorNames that returns a String[] containing the entry
-    //TODO: "[SELECT INSTRUCTOR]" at position 0, followed by all the full instructor names in the
-    //TODO: allInstructorsList
+    //DONE (2): Create a method getInstructorNames that returns a String[] containing the entry
+    //DONE: "[SELECT INSTRUCTOR]" at position 0, followed by all the full instructor names in the
+    //DONE: allInstructorsList
     private String[] getInstructorNames()
     {
         String instructorName[] = new String[allInstructorsList.size() + 1];
@@ -166,22 +174,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //TODO (3): Create a void method named reset that sets the test of the edit text back to an
-    //TODO: empty string, sets the selection of the Spinner to 0 and clears out the offeringListAdapter,
-    //TODO: then rebuild it with the allOfferingsList
+    //DONE (3): Create a void method named reset that sets the test of the edit text back to an
+    //DONE: empty string, sets the selection of the Spinner to 0 and clears out the offeringListAdapter,
+    //DONE: then rebuild it with the allOfferingsList
+    public void reset(View v)
+    {
+        courseTitleEditText.setText("");
+
+        instructorSpinner.setSelection(0);
+
+        offeringsListAdapter.clear();
+        offeringsListAdapter.addAll(allOfferingsList);
+    }
 
 
 
-    //TODO (4): Create a TextWatcher named courseTitleTextWatcher that will implement the onTextChanged method.
-    //TODO: In this method, set the selection of the instructorSpinner to 0, then
-    //TODO: Clear the offeringListAdapter
-    //TODO: If the entry is an empty String "", the offeringListAdapter should addAll from the allOfferingsList
-    //TODO: Else, the offeringListAdapter should add each Offering whose course title starts with the entry.
+
+    //DONE (4): Create a TextWatcher named courseTitleTextWatcher that will implement the onTextChanged method.
+    //DONE: In this method, set the selection of the instructorSpinner to 0, then
+    //DONE: Clear the offeringListAdapter
+    //DONE: If the entry is an empty String "", the offeringListAdapter should addAll from the allOfferingsList
+    //DONE: Else, the offeringListAdapter should add each Offering whose course title starts with the entry.
 
 
 
-    //TODO (5): Create an AdapterView.OnItemSelectedListener named instructorSpinnerListener and implement
-    //TODO: the onItemSelected method to do the following:
-    //TODO: If the selectedInstructorName != "[Select Instructor]", clear the offeringListAdapter,
-    //TODO: then rebuild it with every Offering that has an instructor whose full name equals the one selected.
+    //DONE (5): Create an AdapterView.OnItemSelectedListener named instructorSpinnerListener and implement
+    //DONE: the onItemSelected method to do the following:
+    //DONE: If the selectedInstructorName != "[Select Instructor]", clear the offeringListAdapter,
+    //DONE: then rebuild it with every Offering that has an instructor whose full name equals the one selected.
 }
